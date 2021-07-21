047ff289419a72b7aedb2d6049e5178dfa4d37d3
Maxine Hanrieder <15159137+maxin-e@users.noreply.github.com>
2021-07-21 16:46:15 +0200

    fix LIDO citation extractor

--- data_extraction/citations/rechtspraak_citations_extractor_incremental.py
+++ data_extraction/citations/rechtspraak_citations_extractor_incremental.py
@@ -4,6 +4,8 @@ import urllib.request
 import rdflib
 import os
 import sys
+from os.path import dirname, abspath
+sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
 import csv
 import pandas as pd
 from definitions.file_paths import CSV_CASE_CITATIONS, CSV_LEGISLATION_CITATIONS
@@ -19,19 +21,22 @@ from definitions.file_paths import CSV_CASE_CITATIONS, CSV_LEGISLATION_CITATIONS
 
 def is_valid_ecli_code(ecli):
     ecli_code = ecli.replace(" ","")                                        # Remove spaces
-    ecli_components = ecli_code.split(":")                                  # Split the ECLI into components
-    if ("." in input_eclis or len(ecli_components) != 5):                   # Each ECLI has exactly 5 components (and does not have a full stop - this probably indicates a file name)
+    ecli_components = ecli_code.split(":")                                  # Split the ECLI into utils
+    if ("." in input_eclis or len(ecli_components) != 5):                   # Each ECLI has exactly 5 utils (and does not have a full stop - this probably indicates a file name)
         return False
     if (ecli_components[0].lower() == 'ecli'):                              # The first part of an ECLI is "ECLI"
         return True
     else:
         return False
 
+
 def remove_spaces_from_ecli(ecli):
     return ecli.replace(" ","")
 
+
 def is_valid_csv_file_name(filename):
     return input_eclis[-4:].lower() == ".csv"                               # CSV filename should end in ".csv"
+
 
 def write_data_to_csv(filename,data):
 
@@ -40,26 +45,17 @@ def write_data_to_csv(filename,data):
         writer.writerows(data)
 
 
-def write_incremental_rows(old_citations, old_legislation_citations, new_citations, new_legislation_citations, citation_output, l_citation_output):
-    print("save file")
-    #print("old citation shape : "+str(old_citations.shape))
-    #print("old legislation citation shape : " + str(old_legislation_citations.shape))
+def create_csv(filename, fieldnames):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
 
-    for nc in new_citations:
-        old_citations = old_citations.append(pd.Series(nc, index=old_citations.columns), ignore_index=True)
-
-    for nc in new_legislation_citations:
-        old_legislation_citations = old_legislation_citations.append(pd.Series(nc, index=old_legislation_citations.columns), ignore_index = True)
-
-    #print("new citation shape : " + str(old_citations.shape))
-    #print("new legislation citation shape : " + str(old_legislation_citations.shape))
-
-    old_citations.to_csv(citation_output, index=False)
-    old_legislation_citations.to_csv(l_citation_output,index=False)
-
-    return old_citations, old_legislation_citations
-
+def write_incremental_rows(filename, fieldnames, citations):
+    with open(filename, 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for citation in citations:
+            writer.writerow(dict(zip(fieldnames, citation)))
 
 
 if len(sys.argv) != 6:                                                      # At least 5 arguments should be specified (excluding the script name)
@@ -186,6 +182,11 @@ def is_legislation(sub_ref):
 
 # Extract ECLI code of citation from a lido identifier. Example of a LIDO identifier "https://linkeddata.overheid.nl/terms/bwb/id/BWBR0020368/8655654/2016-08-11/2016-08-11"
 def get_lido_id(ecli):
+    """
+
+    :param ecli:
+    :return:
+    """
     return "http://linkeddata.overheid.nl/terms/jurisprudentie/id/" + ecli
 
 
@@ -209,7 +210,7 @@ def get_last_ecli_downloaded(downloaded_citations):
 
 # Main method to execute LIDO API call on a list of ECLIs from a CSV file and extract the citations of each
 #Add the implementation of the incremental writing of rows
-def find_citations_for_cases(filename, last_ecli, downloaded_citations, downloaded_legislation_citations, case_citations_output_name, legislation_citation_output_name):
+def find_citations_for_cases(filename, last_ecli, case_citations_output_name, case_citations_fieldnames, legislation_citation_output_name, legislation_citations_fieldnames):
     print(filename)
 
     start= False
@@ -247,40 +248,25 @@ def find_citations_for_cases(filename, last_ecli, downloaded_citations, download
     else:
         print("Fetching outgoing citations from LIDO...")
     print()
-    
-    num_eclis = len(eclis)
-    index = 1
 
-    hundred_counts = 3000
-    case_law_result_temp = []
-    legislation_result_temp = []
-    for ecli in eclis:
-        print(remove_spaces_from_ecli(ecli),"\t\t","(",index,"/",num_eclis,")")
-        print()
-        index += 1
-        hundred_counts += 1
+    for i, ecli in enumerate(eclis):
+        case_law_result_temp = []
+        legislation_result_temp = []
+        print(f"Processing {remove_spaces_from_ecli(ecli)} \t\t ({i}/{len(eclis)})")
         current_citations = find_citations_for_case(remove_spaces_from_ecli(ecli))
-
-        #print("citation size : "+str(len(current_citations[0])))
-        #print("legislation citation size : " + str(len(current_citations[1])))
-
         #temporary list of citations (stored but not yet written to the csv file)
         case_law_result_temp.extend(current_citations[0])
+        print(f'case citations: {len(case_law_result_temp)}')
         legislation_result_temp.extend(current_citations[1])
+        print(f'leg. citations: {len(legislation_result_temp)}\n')
 
-
-        #save in csv file when count == 100
-        if hundred_counts == 30:
-            #write them to the csv file
-            downloaded_citations, downloaded_legislation_citations = write_incremental_rows(downloaded_citations, downloaded_legislation_citations, case_law_result_temp, legislation_result_temp, case_citations_output_name, legislation_citation_output_name)
-            #set the hundred ones to 0, and empty the temporary citations list
-            case_law_result_temp = []
-            legislation_result_temp = []
-            hundred_counts = 0
-
-    #write to csv the last batch of cases
-    write_incremental_rows(downloaded_citations, downloaded_legislation_citations, case_law_result_temp,legislation_result_temp, case_citations_output_name, legislation_citation_output_name)
-
+        # append case and legislation citations to corresponding csvs:
+        write_incremental_rows(filename=case_citations_output_filename,
+                               fieldnames=case_citations_fieldnames,
+                               citations=case_law_result_temp)
+        write_incremental_rows(filename=legislation_citations_output_filename,
+                               fieldnames=legislation_citations_fieldnames,
+                               citations=legislation_result_temp)
 
 
 # Main method to execute LIDO API call on the ECLI code of the input case and extract the citations
@@ -402,14 +388,14 @@ else:
         print("citation file already exists, last ecli : " + str(last_ecli))
     #if it doesnt, create it with the right columns
     else:
-        downloaded_citations = pd.DataFrame(columns=['ecli', 'Jurisprudentie'])
+        case_citations_fieldnames = ['ecli', 'Jurisprudentie']
+        create_csv(filename=case_citations_output_filename, fieldnames=case_citations_fieldnames)
 
     if os.path.isfile(legislation_citations_output_filename):
         downloaded_legislation_citations = pd.read_csv(legislation_citations_output_filename)
     else:
-        downloaded_legislation_citations = pd.DataFrame(columns=['ecli', 'Wet', 'Artikel', 'Artikel Title'])
-
-
+        legislation_citations_fieldnames = ['ecli', 'Wet', 'Artikel', 'Artikel Title']
+        create_csv(filename=legislation_citations_output_filename, fieldnames=legislation_citations_fieldnames)
 
     #if the file already existed
     if last_ecli != 0:
@@ -417,14 +403,14 @@ else:
         print("pre size : "+str(downloaded_citations.shape))
         #delte the rows that come from that ecli, so that we dont re-download some of the citations (in case it broke in the middle)
         #if a later case had legislation citations but no case citations, it will be re-downloaded (because we are taking the last ecli
-        #in the case citation table). This is why we have to drop duplicated from the legislation citation table at the very end of the script.
+        #in the case citation dynamodb_table). This is why we have to drop duplicated from the legislation citation dynamodb_table at the very end of the script.
         downloaded_citations = downloaded_citations[downloaded_citations["ecli"] != last_ecli]
         print("post size : " + str(downloaded_citations.shape))
 
         print('legislation citations downloaded shape : '+str(downloaded_legislation_citations.shape))
 
     #find citations, and save the file incrementally
-    find_citations_for_cases(input_eclis, last_ecli, downloaded_citations, downloaded_legislation_citations, case_citations_output_filename, legislation_citations_output_filename)
+    find_citations_for_cases(input_eclis, last_ecli, case_citations_output_filename, case_citations_fieldnames, legislation_citations_output_filename, legislation_citations_fieldnames)
 
     # DROP DUPLICATES from the legislation citation table
     legislation_citations = pd.read_csv(legislation_citations_output_filename)
