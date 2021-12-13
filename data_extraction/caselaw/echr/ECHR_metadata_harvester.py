88d697199f5d6a7f1e8fd55af20852f0aabe82c3
Bogdan Covrig <bogcov@gmail.com>
2021-12-13 13:01:18 +0100

    Add ECHR extraction script

--- data_extraction/caselaw/echr/ECHR_metadata_harvester.py
+++ data_extraction/caselaw/echr/ECHR_metadata_harvester.py
@@ -1,1 +1,136 @@
-# hey
+import requests
+import time
+
+import pandas as pd
+
+
+def read_echr_metadata(start_id=0, end_id=None, fields=None, verbose=True):
+    """
+    Read ECHR metadata into a Pandas DataFrame.
+    :param start_id: result index to start search from
+    :param end_id: result index to end search at (default None fetches all results)
+    :param fields: list of meta attribute names to return (default None fetches all attributes)
+    :param verbose: bool whether or not to print fetched URLs
+    :return:
+    """
+
+    data = []
+    fields = ['itemid',
+              'applicability',
+              'application',
+              'appno',
+              'article',
+              'conclusion',
+              'decisiondate',
+              'docname',
+              'documentcollectionid',
+              'documentcollectionid2',
+              'doctype',
+              'doctypebranch',
+              'ecli',
+              'externalsources',
+              'extractedappno',
+              'importance',
+              'introductiondate',
+              'isplaceholder',
+              'issue',
+              'judgementdate',
+              'kpdate',
+              'kpdateAsText',
+              'kpthesaurus',
+              'languageisocode',
+              'meetingnumber',
+              'originatingbody',
+              'publishedby',
+              'Rank',
+              'referencedate',
+              'reportdate',
+              'representedby',
+              'resolutiondate',
+              'resolutionnumber',
+              'respondent',
+              'respondentOrderEng',
+              'rulesofcourt',
+              'separateopinion',
+              'scl',
+              'sharepointid',
+              'typedescription',
+              'nonviolation',
+              'violation',
+              ]
+    META_URL = 'http://hudoc.echr.coe.int/app/query/results' \
+        '?query=(contentsitename=ECHR) AND ' \
+               '(documentcollectionid2:"JUDGMENTS" OR documentcollectionid2:"COMMUNICATEDCASES") AND' \
+               '(languageisocode:"ENG")' \
+        '&select={select}' + \
+        '&sort=itemid Ascending' + \
+        '&start={start}&length={length}'
+    META_URL = META_URL.replace(' ', '%20')
+    META_URL = META_URL.replace('"', '%22')
+    # example url: "https://hudoc.echr.coe.int/app/query/results?query=(contentsitename=ECHR)%20AND%20(documentcollectionid2:%22JUDGMENTS%22%20OR%20documentcollectionid2:%22COMMUNICATEDCASES%22)&select=itemid,applicability,application,appno,article,conclusion,decisiondate,docname,documentcollectionid,%20documentcollectionid2,doctype,doctypebranch,ecli,externalsources,extractedappno,importance,introductiondate,%20isplaceholder,issue,judgementdate,kpdate,kpdateAsText,kpthesaurus,languageisocode,meetingnumber,%20originatingbody,publishedby,Rank,referencedate,reportdate,representedby,resolutiondate,%20resolutionnumber,respondent,respondentOrderEng,rulesofcourt,separateopinion,scl,sharepointid,typedescription,%20nonviolation,violation&sort=itemid%20Ascending&start=0&length=2"
+
+    # get total number of results:
+    url = META_URL.format(select=','.join(fields), start=0, length=1)
+    r = requests.get(url)
+    resultcount = r.json()['resultcount']
+
+    print(resultcount)
+
+    if not end_id:
+        end_id = resultcount
+    end_id = start_id + end_id
+
+    if start_id+end_id > 500:  # HUDOC does not allow fetching more than 500 items at the same time
+        for i in range(start_id, end_id, 500):
+            print(" - Fetching information from cases {} to {}.".format(i, i+500))
+
+            # Fromat URL based on the incremented index
+            url = META_URL.format(select=','.join(fields), start=i, length=500)
+            if verbose:
+                print(url)
+            r = requests.get(url)
+
+            # Get the results list
+            temp_dict = r.json()['results']
+
+            # Get every doc from the results list
+            for result in temp_dict:
+                data.append(result['columns'])
+    else:
+        # Format URL based on start and length
+        url = META_URL.format(select=','.join(
+            fields), start=start_id, length=end_id)
+        if verbose:
+            print(url)
+        r = requests.get(url)
+
+        # Get the results list
+        temp_dict = r.json()['results']
+
+        # Get every doc from the results list
+        for result in temp_dict:
+            data.append(result['columns'])
+
+    return pd.DataFrame.from_records(data), resultcount
+
+
+print('\n--- START ---')
+start = time.time()
+
+print("--- Extract ECHR data")
+df, resultcount = read_echr_metadata(end_id=103, fields=['itemid', 'documentcollectionid2', 'languageisocode'], verbose=False)
+
+print(df)
+print(f'ECHR data shape: {df.shape}')
+print(f'Columns extracted: {list(df.columns)}')
+
+print("--- Filter ECHR data")
+df_eng = df.loc[df['languageisocode'] == 'ENG']
+
+print(f'df before: {df.shape}')
+print(f'df after: {df_eng.shape}')
+
+end = time.time()
+print("\n--- DONE ---")
+print("Time taken: ", time.strftime('%H:%M:%S', time.gmtime(end - start)))
+
