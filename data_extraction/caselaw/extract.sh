cd683154e2c1f9389089b87295a586067d02f8b4
Maxine Hanrieder <15159137+maxin-e@users.noreply.github.com>
2022-03-08 10:43:58 +0100

    remove unused files

--- data_extraction/caselaw/extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run rechtspraak extraction scripts
-python3 data_extraction/caselaw/rechtspraak/rechtspraak_dump_downloader.py
-python3 data_extraction/caselaw/rechtspraak/rechtspraak_dump_unzipper.py
-python3 data_extraction/caselaw/rechtspraak/rechtspraak_metadata_dump_parser.py
-
-# Run li extraction scripts
-# python3 data_extraction/caselaw/legal_intelligence/legal_intelligence_extractor.py
