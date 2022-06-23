fe2ae30acd017780e22b743400a6ef80dfba3ce6
Cloud956 <pmlewandowski@gmail.com>
2022-06-23 13:07:33 +0200

    csv manipulator working version

--- data_extraction/caselaw/cellar/csv_manipulator.py
+++ data_extraction/caselaw/cellar/csv_manipulator.py
@@ -1,32 +1,45 @@
 import glob, sys
-from os.path import dirname, abspath,join
+from os.path import dirname, abspath
 import pandas as pd
-import csv
 sys.path.append(dirname(dirname(dirname(dirname(abspath(__file__))))))
-from definitions.storage_handler import CELLAR_DIR
-output_path=join(CELLAR_DIR,'output.csv')
+from definitions.storage_handler import DIR_DATA_PROCESSED
 
+# In this list, we would insert headings of all the columns we want removed from the data
 data_to_drop=["CASE LAW BASED ON A LEGAL INSTRUMENT"]
 
 def read_csv(file_path):
-
-    data = pd.read_csv(file_path,sep=",",encoding='utf-8')
-    print(data)
-    return data
+    try:
+        data = pd.read_csv(file_path,sep=",",encoding='utf-8')
+        #print(data)
+        return data
+    except:
+        print("Something went wrong when trying to open the csv file!")
+        sys.exit(2)
 
 def drop_columns(data,columns):
     for i in range(len(columns)):
-        data.pop(columns[i])
-        #data.drop(columns[i],inplace=True,axis=1)
+        try:
+            data.pop(columns[i])
+        except:
+            print(f"Column titled {columns[i]} does not exist in the file!")
+
 
 
 if __name__ == '__main__':
+    print("")
     print("TRANSFORMATION OF CSV FILES IN CELLAR DIR STARTED")
-    csv_files = (glob.glob(CELLAR_DIR + "/" + "*.csv"))
+    print("")
+    csv_files = (glob.glob(DIR_DATA_PROCESSED + "/" + "*.csv"))
     print(f"FOUND {len(csv_files)} CSV FILES")
+
     for i in range(len(csv_files)):
-        print(f"TRANSFORMING {csv_files[i]} ")
-        data=read_csv(csv_files[i])
-        drop_columns(data,data_to_drop)
-        data.to_csv(output_path,index=False)
+        if("Transformed" not in csv_files[i]):
+            print("")
+            print(f"TRANSFORMING {csv_files[i]} ")
+            data=read_csv(csv_files[i])
+            drop_columns(data,data_to_drop)
+            output_path=csv_files[i].replace(".csv","_Transformed.csv")
+            data.to_csv(output_path,index=False)
+    print("")
     print(f"TRANSFORMATION DONE")
+    print("")
