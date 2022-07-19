3fa557822f2bfefb0bc4a672da8a725f497541b5
pranavnbapat <pranav.g33k@gmail.com>
2022-07-19 14:18:10 +0530

    Rechtspraak API comments added. Rechtspraak API metadata fetcher added, not yet finished.

--- data_extraction/caselaw/rechtspraak/rechtspraak_api.py
+++ data_extraction/caselaw/rechtspraak/rechtspraak_api.py
@@ -1,5 +1,5 @@
 import requests, json, xmltodict, pandas as pd, argparse
-from datetime import date
+from datetime import date, datetime
 
 from os.path import dirname, abspath
 import sys
@@ -7,9 +7,11 @@ sys.path.append(dirname(dirname(dirname(dirname(abspath(__file__))))))
 from definitions.storage_handler import DIR_DATA_RECHTSPRAAK
 
 
-
+# Check whether the API is working
 def check_api(url):
     response = requests.get(f"{url}")
+
+    # Return with the response code
     return response.status_code
 
 def get_date(url):
@@ -24,6 +26,7 @@ def get_date(url):
     return json_object
 
 def save_csv(json_object, file_name):
+    # Define the dataframe to enter the data
     df = pd.DataFrame(columns=['id', 'title', 'summary', 'updated', 'link'])
     ecli_id = []
     title = []
@@ -47,13 +50,15 @@ def save_csv(json_object, file_name):
     df['updated'] = updated
     df['link'] = link
 
+    # Save CSV file
     df.to_csv(DIR_DATA_RECHTSPRAAK + "/" + file_name + '.csv', index=False)
 
 if __name__ == '__main__':
+    # Define the base URL
     base_url = "https://data.rechtspraak.nl/uitspraken/zoeken?"
-    url = "https://data.rechtspraak.nl/uitspraken/zoeken?date=2021-01-01&date=2021-12-31"
     print("Rechtspraak dump downloader API")
 
+    # Arguements to pass to API
     parser = argparse.ArgumentParser()
     parser.add_argument('--max', help='Maximum number of ECLIs to retrieve', type=int, required=True)
     parser.add_argument('--starting-date', help='Starting date', required=True)
@@ -63,22 +68,30 @@ if __name__ == '__main__':
     amount = args.max
     starting_date = args.starting_date
 
+    # If end date is not entered, the current date is taken
     today = date.today()
     ending_date = today.strftime("%Y-%m-%d")
-
     if args.ending_date:
         ending_date = args.ending_date
 
+    # Build the URL after getting all the arguements
     url = base_url + 'max=' + str(amount) + '&date=' + starting_date + '&date=' + ending_date
 
     print("Checking the API")
+    # Check the working of API
     response_code = check_api(url)
     if response_code == 200:
         print("API is working fine!")
         print("Getting " + str(amount) + " documents from " + starting_date + " till " + ending_date)
         json_object = get_date(url)
         if json_object:
-            file_name = 'Rechtspraak_' + starting_date + '_' + ending_date
+            # Get current time
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
+            # Make file name
+            file_name = 'Rechtspraak_' + starting_date + '_' + ending_date + '_' + current_time
+
             save_csv(json_object, file_name)
             print("Data saved to CSV file successfully.")
     else:
