c5b2a564ec7707d462b5e0be26f72f9a91b115a9
Piotr Lewandowski <pmlewandowski@gmail.com>
2022-10-07 13:14:36 +0200

    full text saving separation, testing file comments and links moved to .env

--- airflow/dags/helpers/Testing_file.py
+++ airflow/dags/helpers/Testing_file.py
@@ -1,40 +1,28 @@
+"""
+
+This file is purely a testing file for trying out separate parts of code, testing if everything works and such.
+Can be also used to develop future code.
+
+Current main usage - Setting up Storage -> Setting up all the folders in root directory.
+
+
+
+"""
+
+
+
+
+
 from helpers.csv_manipulator import *
 from helpers.citations_adder import *
-#from helpers.fulltext_saving import *
+from helpers.fulltext_saving import *
 from helpers.json_to_csv import *
 from helpers.sparql import *
-def get_eurovoc(text):
-    try:
-        start = text.find("EUROVOC")
-        try:
-            ending=text.find("Subject matter")
-        except:
-            try:
-                ending=text.find("Directory code")
-            except:
-                try:
-                    ending=text.find("Miscellaneous information")
-                except:
-                    ending=start
-        if ending is start:
-            return ""
-        else:
-            text=text[start:ending]
-            texts = text.split("\n")
-            lists = []
-            for t in texts:
-                if "EUROVOC" not in t and t != "":
-                    lists.append(t)
-            return "_".join(lists)
-    except:
-        return ""
+from definitions.storage_handler import Storage
 if __name__ == '__main__':
 
     #link = "https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:52018IP0428&qid=1662057930872"
     #html=response_wrapper(link)
    # text = get_full_text_from_html(html.text)
     #print(get_eurovoc(text))
-    word="sdasda11"
-    print(word[-1])
-    word=word[:-1]
-    print(word)
+    storage=Storage("local")
