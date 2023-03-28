e8c9921ccc46f0b379e59d55bf2fa830156f8ce0
Piotr Lewandowski <pmlewandowski@gmail.com>
2023-03-28 15:16:02 +0200

    small patch

--- airflow/dags/data_loading/data_loader.py
+++ airflow/dags/data_loading/data_loader.py
@@ -1,30 +1,31 @@
 # %%
 from os.path import dirname, abspath, basename
 import sys
+
 sys.path.append(dirname(dirname(abspath(__file__))))
 import os
 from csv import DictReader
-import csv, json
-from data_loading.row_processors.dynamodb import DynamoDBRowProcessor,DynamoDBFullTextProcessor,DynamoDBRowCelexProcessor
-from data_loading.row_processors.opensearch import OpenSearchRowProcessor
+import csv
+from data_loading.row_processors.dynamodb import DynamoDBRowProcessor, DynamoDBRowCelexProcessor
 from data_loading.clients.dynamodb import DynamoDBClient
-from data_loading.clients.opensearch import OpenSearchClient
-from definitions.storage_handler import Storage, CSV_RS_CASES, CSV_LI_CASES, CSV_RS_OPINIONS, CSV_CASE_CITATIONS, \
-    CSV_LEGISLATION_CITATIONS, get_path_processed, get_path_raw,CSV_CELLAR_CASES,CSV_ECHR_CASES, JSON_FULL_TEXT_CELLAR, \
+from definitions.storage_handler import Storage, CSV_RS_CASES, CSV_RS_OPINIONS, \
+    get_path_processed, CSV_CELLAR_CASES, CSV_ECHR_CASES, \
+    JSON_FULL_TEXT_CELLAR, \
     JSON_FULL_TEXT_ECHR
 from fulltext_bucket_saving import upload_fulltext, bucket_name
 import time
 import argparse
 from ctypes import c_long, sizeof
 from dotenv import load_dotenv
+
 load_dotenv()
-#csv.field_size_l
+# csv.field_size_l
 # imit(sys.maxsize) #appears to be system dependent so is replaced with:
-#from https://stackoverflow.com/questions/52475749/maximum-and-minimum-value-of-c-types-integers-from-python
+# from https://stackoverflow.com/questions/52475749/maximum-and-minimum-value-of-c-types-integers-from-python
 signed = c_long(-1).value < c_long(0).value
-bit_size = sizeof(c_long)*8
-signed_limit = 2**(bit_size-1)
-csv.field_size_limit(signed_limit-1 if signed else 2*signed_limit-1)
+bit_size = sizeof(c_long) * 8
+signed_limit = 2 ** (bit_size - 1)
+csv.field_size_limit(signed_limit - 1 if signed else 2 * signed_limit - 1)
 
 
 def load_data(argv):
@@ -52,15 +53,14 @@ def load_data(argv):
     args = parser.parse_args(argv)
     storage = Storage(location=args.storage)
     print(f'INPUT/OUTPUT DATA STORAGE FOR METADATA: {args.storage}')
-    print('INPUT/OUTPUT DATA STORAGE FOR  FULL TEXT:\t',bucket_name)
-  
+    print('INPUT/OUTPUT DATA STORAGE FOR  FULL TEXT:\t', bucket_name)
+
     print('INPUT:\t\t\t\t', [basename(input_path) for input_path in input_paths])
 
     # set up clients
     ddb_client_ecli = DynamoDBClient(os.getenv('DDB_TABLE_NAME'))
     ddb_client_celex = DynamoDBClient(os.getenv('DDB_TABLE_NAME_CELEX'))
     ddb_full_text_client = DynamoDBClient(os.getenv('DDB_FULL_TEXT_TABLE_NAME'))
-
 
     # evaluate input arguments
     if args.delete == 'ddb':
@@ -89,34 +89,35 @@ def load_data(argv):
             ddb_item_counter = 0
             os_item_counter = 0
             if get_path_processed(CSV_CELLAR_CASES) in input_path:
-                ddb_rp = DynamoDBRowCelexProcessor(input_path,ddb_client_celex.table)
+                ddb_rp = DynamoDBRowCelexProcessor(input_path, ddb_client_celex.table)
             else:
-                ddb_rp = DynamoDBRowProcessor(input_path,ddb_client_ecli.table)
+                ddb_rp = DynamoDBRowProcessor(input_path, ddb_client_ecli.table)
             # process csv by row
-            with open(input_path, 'r', newline='',encoding="utf8") as in_file:
+            with open(input_path, 'r', newline='', encoding="utf8") as in_file:
                 reader = DictReader(in_file)
                 for row in reader:
-                     # skip empty rows and remove empty attributes
-                     if row != '':
+                    # skip empty rows and remove empty attributes
+                    if row != '':
                         # atts = list(row.items())
                         # for att in atts:
-                            # if att[1] == '':
-                            #     row.pop(att[0])
+                        # if att[1] == '':
+                        #     row.pop(att[0])
                         # process row
                         ddb_item_counter += ddb_rp.upload_row(row)
 
-                    # log progress
+                        # log progress
                         case_counter += 1
                         if case_counter % 1000 == 0:
                             print(case_counter, 'rows processed.')
 
             print(f'{case_counter} cases ({ddb_item_counter} ddb items and {os_item_counter} os items) added.')
-            if args.storage =="aws":
+            if args.storage == "aws":
                 os.remove(input_path)
-        upload_fulltext(storage=args.storage,files_location_paths=full_text_paths)
-    end = time.time()         # celex, item_id
+        upload_fulltext(storage=args.storage, files_location_paths=full_text_paths)
+    end = time.time()  # celex, item_id
     print("\n--- DONE ---")
     print("Time taken: ", time.strftime('%H:%M:%S', time.gmtime(end - start)))
+
 
 if __name__ == '__main__':
     load_data(sys.argv[1:])
