b76d267dbf9c80c5f2965e8f7178eb00df944e65
shashankmc <3445114+shashankmc@users.noreply.github.com>
2026-03-31 15:52:43 +0200

    feat: add maintenance DAGs for citation updates and log cleanup

---
+++ airflow/dags/maintenance/log_cleaner/dag.py
@@ -0,0 +1,22 @@
+"""
+EC2 log cleanup DAG.
+This dag cleans up all of the logs on the EC2 instance, as logs tend up to get quite big in size.
+"""
+
+from datetime import datetime
+
+from airflow.operators.python import PythonOperator
+from shared.helpers.log_cleanup import log_clean
+
+from airflow import DAG
+
+default_args = {"owner": "airflow"}
+
+with DAG(
+    dag_id="log_cleaner",
+    default_args=default_args,
+    description="fully implemented",
+    start_date=datetime.now(),
+    schedule_interval="@monthly",
+) as DAG:
+    task1 = PythonOperator(task_id="log_cleaner", python_callable=log_clean)
